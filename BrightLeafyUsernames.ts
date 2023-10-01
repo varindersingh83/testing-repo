08ac8b17ff158f4e9b335496d3377e8fd3ceb2fc
Varinder Singh <varinder_singh3@apple.com>
2023-10-01 12:27:08 -0700

    Conditions and loops done

--- BrightLeafyUsernames.ts
+++ BrightLeafyUsernames.ts
@@ -1,6 +1,13 @@
 // Buliding an interest rate calculator
-console.log("############################\n");
-console.log("We are in this file.\nHello !");
+
+// shortcut vs code
+// code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf
+
+// console.log("############################\n");
+// console.log("We are in this file.\nHello !");
+
+// import "inquirer";
+
 let amount = "A";
 let principal = "P";
 let rate = "r";
@@ -14,15 +21,50 @@ const annualIncome = 50000;
 const monthlyRent = 2000;
 const monthlyIncome = annualIncome / MONTH_IN_YEAR;
 const monthlySaving = monthlyIncome - monthlyRent;
-console.log(`monthly income = ${monthlyIncome.toFixed(2)}`);
+// console.log(`monthly income = ${monthlyIncome.toFixed(2)}`);
 const annualTaxPercentage = 0.33;
 const annualTaxDollars = annualIncome * annualTaxPercentage;
 const annualIncomeWithTaxDeducted =
   annualIncome - annualTaxPercentage * annualIncome;
-console.log(
-  `annual income with tax deducted = ${annualIncomeWithTaxDeducted.toFixed(2)}`
-);
+// console.log(
+//   `annual income with tax deducted = ${annualIncomeWithTaxDeducted.toFixed(2)}`
+// );
 // conditions are needed in java script 'a>b, a<b, a!=b, etc
+
+console.log(10 > 5 && 7 > 1); // true
+console.log(10 < 5 && 7 > 1); // false
+console.log(10 > 5 && 7 < 110); // true
+
+const ELIGIBLE_AGE = 21;
+let yourAge = 18;
+
+if (yourAge >= ELIGIBLE_AGE) {
+  console.log("Welcome to the Bar!");
+} else {
+  console.log("Sorry! you are not allowed to enter the Bar.");
+}
+
+console.log(`your age = ${yourAge}`);
+while (yourAge < ELIGIBLE_AGE) {
+  yourAge += 1;
+  console.log(`your age is increased = ${yourAge}`);
+}
+
+if (yourAge >= ELIGIBLE_AGE) {
+  console.log("Welcome to the Bar!");
+} else {
+  console.log("Sorry! you are not allowed to enter the Bar.");
+}
+
+//
+let str = "";
+for (let i = 0; i < 9; i++) {
+  str = str + "#";
+  console.log(str);
+}
+
+// let yourAge = prompt("What is your age?");
+
 /*
 add tax percentage  = 33
 display income with tax deducted 
